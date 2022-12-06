485490c3335648398ecf08ec182e80b1759ad6da
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2022-12-06 14:47:45 +0200

    Доопрацювання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,36 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // console.log(galleryItems);
 const galleryEl = document.querySelector('.gallery');
-const cardsImg = createCardsImgMarkup(galleryItems);
+// const cardsImg = createCardsImgMarkup(galleryItems);
 // console.log(galleryEl);
 function createCardsImgMarkup(img) {
   return img
     .map(({ preview, original, description }) => {
       return `<a class="gallery__item" href='${original}'>
-            <img
-             class="gallery__image"
-             src='${preview}'
-             alt='${description}'/>
-        </a>`;
+                  <img
+                    class="gallery__image"
+                    src='${preview}'
+                    alt='${description}'/>
+               </a>`;
     })
     .join('');
 }
 // console.log(imagesMarkup);
-galleryEl.insertAdjacentHTML('afterbegin', cardsImg);
+galleryEl.insertAdjacentHTML('afterbegin', createCardsImgMarkup(galleryItems));
 
-galleryEl.addEventListener('click', onClickGalleryImg);
+// galleryEl.addEventListener('click', onClickGalleryImg);
 
-function onClickGalleryImg(evt) {
-  evt.preventDefault();
-  //   console.log(evt);
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-  //   console.log(evt.target);
-  //   console.log(evt.target.alt);
-  //   console.log(evt.target.getAttribute('alt'));
-  var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: `alt`,
-    captionDelay: 250,
-  });
-}
+// function onClickGalleryImg(evt) {
+//   evt.preventDefault();
+//   //   console.log(evt);
+//   if (!evt.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+//   //   console.log(evt.target);
+//   //   console.log(evt.target.alt);
+//   //   console.log(evt.target.getAttribute('alt'));
+// }
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
